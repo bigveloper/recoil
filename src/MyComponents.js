29b6d905215bbdb8eb658c9837f433dc17b04091
bigveloper <bigveloper@gmail.com>
2021-09-28 18:48:16 +0900

    add defaultProps name2

--- src/MyComponents.js
+++ src/MyComponents.js
@@ -1,9 +1,9 @@
 const MyComponent = (props) => {
-    const { name, children } = props;
+    const { name, name2, children } = props;
 
     return (
         <div>
-            안녕하세요. 제 이름은 {name} 입니다. <br />
+            안녕하세요. 제 이름은 {name} 입니다. <br />제 별명은 {name2} 이구요. <br />
             children 값은 {children}
             입니다.
         </div>
@@ -11,6 +11,7 @@ const MyComponent = (props) => {
 };
 MyComponent.defaultProps = {
     name: '기본 이름',
+    name2: '오징어',
 };
 
 export default MyComponent;
