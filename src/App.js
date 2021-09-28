6431e12c540782ad21f3edf74cb2a3df6a7ed27d
bigveloper <bigveloper@gmail.com>
2021-09-28 18:41:55 +0900

    children

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import MyComponent from './MyComponents';
 
 const App = () => {
-    return <MyComponent />;
+    return <MyComponent>리액트</MyComponent>;
 };
 
 export default App;
